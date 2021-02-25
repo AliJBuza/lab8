72385a48570a36fd1c3747f61d795b58f6f4ddd0
Ali Buzayan <abuzayan@ucsd.edu>
2021-02-25 10:41:09 -0800

    got through lab!

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'GTTQ2Vih8zGikuMttraYrlXU5sBHhyHu';
+
+// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [32.88, -117.236],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12,
+	  zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
